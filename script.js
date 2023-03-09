efdd17c16e40f83e6d5a608b8d861ae36ac8ed26
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-03-09 13:45:49 +0100

    podstawa

---
+++ script.js
@@ -0,0 +1,10 @@
+class Auto {
+  constructor(kolor, maxszybkosc) {
+    this.biegow = 6;
+    this.bieg = 0;
+    this.kolor = kolor;
+    this.maxszybkosc = maxszybkosc;
+    this.predkosc = 0;
+    this.silnik = false;
+  }
+}
